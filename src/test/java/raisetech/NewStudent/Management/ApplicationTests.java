3b5656ce40049ce1a93d77287354f3c6469a0f3d
yuka2349 <nakamura.yuka@link-academy.co.jp>
2025-06-04 11:05:44 +0900

    NewStudentManagement最初のコミットプッシュ

---
+++ src/test/java/raisetech/NewStudent/Management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.NewStudent.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
