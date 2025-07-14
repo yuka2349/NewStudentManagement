3686ce97e7162cf1dcc1b16e80c999246539653a
yuka2349 <nakamura.yuka@link-academy.co.jp>
2025-07-14 22:23:37 +0900

    Javaスタンダード12回　演習課題まで

--- src/main/java/raisetech/NewStudent/Management/controller/StudentController.java
+++ src/main/java/raisetech/NewStudent/Management/controller/StudentController.java
@@ -1,11 +1,15 @@
 package raisetech.NewStudent.Management.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.NewStudent.Management.controller.converter.StudentConverter;
 import raisetech.NewStudent.Management.data.Student;
 import raisetech.NewStudent.Management.data.StudentCourses;
+import raisetech.NewStudent.Management.domain.StudentDetail;
 import raisetech.NewStudent.Management.repository.StudentRepository;
 import raisetech.NewStudent.Management.service.StudentService;
 
@@ -13,29 +17,37 @@ import raisetech.NewStudent.Management.service.StudentService;
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
   private StudentRepository studentRepository;
+  private StudentConverter studentConverter;
 
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service,StudentConverter converter) {
     this.service = service;
+    this.converter=converter;
   }
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentCourses> studentCourses = service.searchStudentCourseList();
+
+    return converter.convertStudentDetails(students, studentCourses);
   }
+
+
 
   @GetMapping("/studentCourseList")
   public List<StudentCourses> getStudentCourseList() {
     return service.searchStudentCourseList();
   }
-
+//30代だけの抽出
   @GetMapping("/students/30s")
   public List<Student> getStudentsIn30s() {
     return studentRepository.findStudentsIn30s();
   }
-
+//Javaコースだけの表示
   @GetMapping("/courses/java")
   public List<StudentCourses> getJavaCourses() {
     return studentRepository.findJavaCourses();
---
+++ src/main/java/raisetech/NewStudent/Management/controller/converter/StudentConverter.java
@@ -0,0 +1,29 @@
+package raisetech.NewStudent.Management.controller.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+import raisetech.NewStudent.Management.data.Student;
+import raisetech.NewStudent.Management.data.StudentCourses;
+import raisetech.NewStudent.Management.domain.StudentDetail;
+
+@Component
+public class StudentConverter {
+
+  public List<StudentDetail> convertStudentDetails(List<Student> students,
+      List<StudentCourses> studentCourses) {
+    List<StudentDetail> studentDetails = new ArrayList<>();
+    students.forEach(student -> {
+      StudentDetail studentDetail = new StudentDetail();
+      studentDetail.setStudent(student);
+
+      List<StudentCourses> convertStudentCourses = studentCourses.stream()
+          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
+          .collect(Collectors.toList());
+      studentDetail.setStudentCourses(convertStudentCourses);
+      studentDetails.add(studentDetail);
+    });
+    return studentDetails;
+  }
+}
